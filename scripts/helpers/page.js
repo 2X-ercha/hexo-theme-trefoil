5e3eeb69ebef4c98650ffe3747876512539e6cfd
Mufanc <mufanc.xyz@gmail.com>
2022-12-31 23:29:42 +0800

    style: re-indent files

--- scripts/helpers/page.js
+++ scripts/helpers/page.js
@@ -3,5 +3,5 @@
 const crypto = require('crypto')
 
 hexo.extend.helper.register('md5', function (path) {
-    return crypto.createHash('md5').update(decodeURI(this.url_for(path))).digest('hex')
-  })
+  return crypto.createHash('md5').update(decodeURI(this.url_for(path))).digest('hex')
+})
