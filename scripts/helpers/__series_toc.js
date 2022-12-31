33eff11af1b622566e74a4b30ccf83ee8b2d3caf
noionion <noionion@outlook.com>
2022-12-31 20:25:29 +0800

    refactor: code style

--- scripts/helpers/__series_toc.js
+++ scripts/helpers/__series_toc.js
@@ -1,8 +1,6 @@
 'use strict'
 
-hexo.extend.helper.register('__series_toc', (page) => {
-    const toc = hexo.extend.helper.get('toc').bind(hexo)
-    const url_for = hexo.extend.helper.get('url_for').bind(hexo);
+hexo.extend.helper.register('__series_toc', function (page) {
     const series_dist = hexo.locals.get('series_dist')
     var toc_str = ''
     if (typeof(page.series_name) === 'string' && page.series_index !== 0 && !isNaN(page.series_index)) {
@@ -11,13 +9,13 @@ hexo.extend.helper.register('__series_toc', (page) => {
       for (const it of Object.values(series.pages)) {
         if (page.series_index == it.series_index) {
           toc_str += '<div class="doc-toc-tree-block active">'
-            toc_str += '<a class="doc-toc-tree-h1" href='+url_for(it.path)+'>'
+            toc_str += '<a class="doc-toc-tree-h1" href='+this.url_for(it.path)+'>'
               toc_str += '<span class="toc-text">'+it.subtitle+'</span></a>'
-            toc_str += toc(page.content)
+            toc_str += this.toc(page.content)
           toc_str += '</div>'
         } else {
           toc_str += '<div class="doc-toc-tree-block">'
-            toc_str += '<a class="doc-toc-tree-h1" href='+url_for(it.path)+'>'
+            toc_str += '<a class="doc-toc-tree-h1" href='+this.url_for(it.path)+'>'
               toc_str += '<span class="toc-text">'+it.subtitle+'</span></a></div>'
         }
       }
