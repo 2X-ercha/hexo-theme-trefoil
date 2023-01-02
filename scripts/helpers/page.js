faa2b41a03ea10f290d8c066c1820ccf39c2b8fa
noionion <noionion@outlook.com>
2023-01-02 22:13:09 +0800

    perf: improve head

--- scripts/helpers/page.js
+++ scripts/helpers/page.js
@@ -1,7 +1,16 @@
 'use strict'
 
 const crypto = require('crypto')
+const { prettyUrls } = require('hexo-util')
 
 hexo.extend.helper.register('md5', function (path) {
   return crypto.createHash('md5').update(decodeURI(this.url_for(path))).digest('hex')
+})
+
+hexo.extend.helper.register('injectHtml', function (data) {
+  return data ? Object.values(data).join('') : ''
+})
+
+hexo.extend.helper.register('urlNoIndex', function (url = null) {
+  return prettyUrls(url || this.url, { trailing_index: false, trailing_html: false })
 })
