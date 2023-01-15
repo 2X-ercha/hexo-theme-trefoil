d3e1f9d1aa1f014625d1e7c737ab09c1f4654802
noionion <noionion@outlook.com>
2023-01-15 22:56:21 +0800

    feat: update tag-copy

--- scripts/tags/copy.js
+++ scripts/tags/copy.js
@@ -4,14 +4,12 @@ const { trim } = require('./utils');
 
 hexo.extend.tag.register('copy', function(args) {
   args = hexo.args.map(args, [], ['text'])
-  const html = []
-  html.push(trim`
+  const html = trim`
   <div class="tag-copy">
     <input readonly class="copy-area" value="${args.text}">
     <button class="copy-btn click-limit">
       <i class="ri-file-copy-fill"></i>
     </button>
-  </div>`)
-  console.log(html.join(''))
-  return html.join('')
+  </div>`
+  return html
 })
